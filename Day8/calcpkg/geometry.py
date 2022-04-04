183f87ab8f04761782f4a3635207fb71a7450241
Cheolkie Kim <ckiekim@naver.com>
2022-04-04 22:02:57 +0900

    심사문제 38-45

--- Day8/calcpkg/geometry.py
+++ Day8/calcpkg/geometry.py
@@ -1,5 +1,10 @@
+import math
+
 def triangle_area(base, height):
     return base * height / 2
  
 def rectangle_area(width, height):
     return width * height
+
+def circle_area(radius):
+    return math.pi * radius * radius
--- Day8/calcpkg/operation.py
+++ Day8/calcpkg/operation.py
@@ -1,5 +1,10 @@
+import math
+
 def add(a, b):
     return a + b
  
 def mul(a, b):
     return a * b
+
+def squareroot(n):
+    return math.sqrt(n)
